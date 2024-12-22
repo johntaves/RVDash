7f20752bd6a1e5302d2149889e20b6e68f3cfd02
John Taves <john.taves@pickatime.com>
2024-12-22 14:11:22 -0800

    commented out brightness

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -737,8 +737,6 @@ public class Msg
 }
 public class Gauges : INotifyPropertyChanged
 {
-	private PhysicalMonitorBrightnessController bc = new();
-
 	// boiler-plate
 	public event PropertyChangedEventHandler PropertyChanged;
     protected virtual void OnPropertyChanged(string propertyName)
@@ -942,11 +940,12 @@ public class Gauges : INotifyPropertyChanged
 		get => _lowairsec;
 		set => SetField(ref _lowairsec, value, "lowairsec");
 	}
+    /*
     public uint brightness
     {
-        get => (uint)bc.Get();
+        get => bc.Get();
         set => bc.Set(value);
-    }
+    }*/
     private string _lowwater = "Hidden";
     public string lowwater
     {
@@ -1089,6 +1088,48 @@ internal static class NativeMethods
 		EsDisplayRequired = 0x00000002,
 		EsSystemRequired = 0x00000001
 	}
+}
+/*
+public class GammaThing
+{
+    [DllImport("user32.dll")]
+    private static extern IntPtr GetDC(IntPtr hWnd);
+
+    [DllImport("gdi32.dll")]
+    private static extern bool SetDeviceGammaRamp(IntPtr hDC, ref RAMP lpRamp);
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+    public struct RAMP
+    {
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 256)]
+        public UInt16[] Red;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 256)]
+        public UInt16[] Green;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 256)]
+        public UInt16[] Blue;
+    }
+    private uint val = 0;
+    public uint Get() => val;
+    public void Set(uint gamma)
+    {
+        val = gamma;
+        if (gamma <= 256 && gamma >= 1)
+        {
+            RAMP ramp = new RAMP();
+            ramp.Red = new ushort[256];
+            ramp.Green = new ushort[256];
+            ramp.Blue = new ushort[256];
+            for (uint i = 1; i < 256; i++)
+            {
+                uint iArrayValue = i * (gamma + 128);
+
+                if (iArrayValue > 65535)
+                    iArrayValue = 65535;
+                ramp.Red[i] = ramp.Blue[i] = ramp.Green[i] = (ushort)iArrayValue;
+            }
+            SetDeviceGammaRamp(GetDC(IntPtr.Zero), ref ramp);
+        }
+    }
 }
 
 public class PhysicalMonitorBrightnessController : IDisposable
@@ -1266,3 +1307,4 @@ public class PhysicalMonitorBrightnessController : IDisposable
     }
     #endregion
 }
+*/
